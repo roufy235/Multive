bb5404b77c8c2a374239155623278d8494d621eb
roufy235 <roufy235@gmail.com>
2021-03-27 10:24:44 +0100

    Updated dependencies and csrf

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,4 +1,4 @@
 <script>
     const apiBaseUrl = '<?php echo getBasePath(true); ?>'
 </script>
-<script src="<?php echo getBasePath(); ?>/dist/_9550808079.js?version=<?php echo $_ENV['ADMIN_JAVASCRIPT_VERSION_CONTROL']; ?>"></script>
+<script src="<?php echo getBasePath(); ?>/dist/_9550808079.js?version=<?php echo $_ENV['JAVASCRIPT_VERSION_CONTROL']; ?>"></script>
