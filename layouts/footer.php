045d35d47c7fc93dee141cff59f305ee7f0f076b
roufy235 <roufy235@gmail.com>
2021-04-18 11:11:52 +0100

    Bug fixes

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,4 +1,12 @@
 <script>
-    const apiBaseUrl = '<?php echo getBasePath(true); ?>'
+    const domain = '<?php echo $_ENV['PROJECT_WEB_DOMAIN_URL']; ?>'
+    const token = localStorage.getItem('token') ? localStorage.getItem('token') : '';
+    const axiosInstance = axios.create({
+        baseURL: '<?php echo getBasePath(true); ?>',
+        time: 10000,
+        headers: {
+            'Authorization' : 'Bearer ' + token
+        }
+    });
 </script>
 <script src="<?php echo getBasePath(); ?>/dist/_9550808079.js?version=<?php echo $_ENV['JAVASCRIPT_VERSION_CONTROL']; ?>"></script>
